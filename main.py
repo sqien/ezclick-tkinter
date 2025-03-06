10c55bc12f9a3da2eda7fe35cd1fee87c4bb73e2
sqien <twitchsqien@gmail.com>
2025-03-06 10:56:58 +0100

    updated

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ import threading
 import keyboard
 
 
+
+global hotkey
+hotkey = "ctrl+q"
+
 def clicks_get():
     global clicksAmount
     clicksAmount = clicksAmount_var.get()
-    print(clicksAmount)  
-    
+    print(clicksAmount)
 
 def toggleON():
     pyautogui.click(clicks=clicksAmount)
 
 def listen_hotkey():
-    keyboard.wait("ctrl+q")
+    keyboard.wait(hotkey)
     # clicks_get()
     toggleON()
     listen_hotkey()
@@ -33,9 +36,13 @@ clicksAmount_var = tk.IntVar()
 enterClicks_Label = tk.Label(root,text="Enter amount of clicks").pack()
 enterClicks_Entry = tk.Entry(root, textvariable=clicksAmount_var).pack()
 enterClicks_Submit = tk.Button(root,text="Submit", command=clicks_get).pack()
-exitButton = tk.Button(root, text="Exit", command=sys.exit).pack()
 
+enterHotkey_Label = tk.Label(root, text=f"Your hotkey is: {hotkey} ").pack()
+
+
+exitButton = tk.Button(root, text="Exit", command=sys.exit).pack()
 
 thread = threading.Thread(target=listen_hotkey, daemon=True)
 thread.start()
+
 root.mainloop()
